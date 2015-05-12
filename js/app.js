dc4ef0f7bd25db2ab7e23add359ddda7be634a52
jmartinez <jmartinez@uic.es>
2015-05-12 12:37:58 +0200

    Add review's data creation

--- js/app.js
+++ js/app.js
@@ -32,8 +32,8 @@
     this.review = {};
 
     this.addReview = function(product) {
+      this.review.createdOn = Date.now();
       product.reviews.push(this.review);
-
       this.review = {};
     };
   });
